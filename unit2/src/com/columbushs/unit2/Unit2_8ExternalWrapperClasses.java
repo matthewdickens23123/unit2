af9c6fa81b26a846384e814090dd7b0991362358
IgnacioSoto <isoto@columbushs.com>
2021-09-13 08:46:59 -0400

    adding 2_9

--- unit2/src/com/columbushs/unit2/Unit2_8ExternalWrapperClasses.java
+++ unit2/src/com/columbushs/unit2/Unit2_8ExternalWrapperClasses.java
@@ -14,7 +14,12 @@ public class Unit2_8ExternalWrapperClasses {
 	System.out.println(intObject);
 	System.out.println(intObject.compareTo(159)); //1 if lower, 0 if the same, -1 if higher
 	System.out.println(intObject.intValue());
+	
+	//get a primitive from the object.
+	int backToInt = intObject.intValue();
+	System.out.println(backToInt);
 	}
+	
 	
 	
 	//double primitive data type vs objects.
@@ -29,6 +34,29 @@ public class Unit2_8ExternalWrapperClasses {
 		Double doubleObject = new Double(45.23);
 		System.out.println(doubleObject);
 		System.out.println(doubleObject.equals(23.5));
+		
+		//get a primitive from the object.
+		double backToDouble = doubleObject.doubleValue();
+		System.out.println(backToDouble);
+	}
+	
+	
+	public void otherWrappers() {
+	//not needed for AP test but might be good to know in case you see them.
+	
+	//boolean - true or false
+	boolean d = true;
+	Boolean boolObj = new Boolean(false);
+	
+	//float is a smaller double, 32 bit instead of 64, primitive ends with an f
+	float dec = 123.714f;
+	Float decim2 = new Float(23.5634); // object doesnt neeed the f
+	
+	//char obj
+	char letter = 'f';
+	Character letterObj = new Character('g');
+	
+			
 	}
 	
 }
